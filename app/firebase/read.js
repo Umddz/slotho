32f4d712b0205dc74f46117dbd9f4d8eb3d1d116
Sadness7311 <123866496+Sadness7311@users.noreply.github.com>
2023-09-13 11:40:36 +0500

    Delete app directory

--- app/firebase/read.js
+++
@@ -1,7 +0,0 @@
-import { db } from "./main"
-import { doc, getDoc } from 'firebase/firestore'
-
-export default async function read(collectionID, docID) {
-    const docSnap = await getDoc(doc(db, collectionID, docID))
-    return docSnap.data()
-}
