7cdf8ea2e81cb11c2a1706d7d81b153a4a1237ff
Sadness7311 <123866496+Sadness7311@users.noreply.github.com>
2023-09-16 18:24:35 +0500

    Delete app directory

--- app/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-export default function Input({ label, placeholder }) {
-    return (
-        <div className="input">
-            <h2>{ label }</h2>
-            <input type="text" placeholder={placeholder} />
-        </div>
-    )
-}
--- app/functions/useUser.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-export default function useUser() {
-    const [user, setUser] = useState(sessionStorage.getItem('user-allowed'))
-    const interval = setInterval(() => user === null ? update() : clearInterval(interval), 200)
-
-    const update = () => setUser(sessionStorage.getItem('user-allowed'))
-
-    return user
-}
