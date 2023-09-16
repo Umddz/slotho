bf69a38013748a53b91efdc6f034c8894efe1a45
Sadness7311 <123866496+Sadness7311@users.noreply.github.com>
2023-09-16 12:08:40 +0500

    Delete app directory

--- app/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import Image from 'next/image'
-export default function Logo() {
-    return (
-        <div className="logo">
-            <Image src="/logo.png" alt="Logo" width={100} height={100} />
-        </div>
-    )
-}
--- app/firebase/add.js
+++
@@ -1,6 +0,0 @@
-import { db } from "./main"
-import { doc, setDoc } from 'firebase/firestore'
-
-export default async function add(collection, docID, data) {
-    await setDoc(doc(db, collection, docID), data)
-}
