536af313aa83200ed4d4a3cb40c5eaf3958aa64e
Sadness7311 <123866496+Sadness7311@users.noreply.github.com>
2023-09-16 11:46:58 +0500

    Update MainHeading.jsx

--- app/components/MainHeading.jsx
+++ app/components/MainHeading.jsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import { useContext, useRef } from 'react' 
 import { userContext } from '../contexts/userContext'
 import useWidth from '../functions/useWidth'
-import Image from 'next/image'
 
 export default function MainHeading() {
     const router = useRouter()
@@ -21,7 +20,7 @@ export default function MainHeading() {
             <h3>Server Owner? <button onClick={() => userData ? router.push('/dashboard') : document.querySelector('.nav button').click()}>Add your server</button></h3>
             
             <img src="/sloth.png" alt="Sloth" />
-            <Image src="/discord.png" alt="" className="discord" width={200} height={200}/>
+            <img src="/discord.png" alt="discord" className="discord" />
             
             {width < 1000 && userData && <div className="user-con-m">
                 <h2>Go to the <Link href={`/dashboard`}>Dashboard</Link></h2>
